65c15338076169c2ee60e5a43289c0870b481bf9
Bryan Powell <bryan@metabahn.com>
2023-05-02 21:37:59 -0700

    Update the server example to actually make requests

--- examples/server.rb
+++ examples/server.rb
@@ -8,9 +8,9 @@ server.start
 sleep(1)
 
 puts "making requests..."
-# puts "got: #{HTTP.get("http://localhost:4242").status}"
-# puts "got: #{HTTP.get("http://localhost:4242").status}"
-# puts "got: #{HTTP.get("http://localhost:4242").status}"
+puts "got: #{HTTP.get("http://localhost:4242").status}"
+puts "got: #{HTTP.get("http://localhost:4242").status}"
+puts "got: #{HTTP.get("http://localhost:4242").status}"
 
 puts "shutting down..."
 server.stop
